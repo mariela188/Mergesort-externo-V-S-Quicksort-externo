e43731f58aa48a58d34641dcb1d08b6513f7b016
Mariela Cornejo <mariela.cornejo@ug.uchile.cl>
2025-05-01 19:08:34 -0400

    change quicksort.c

--- code/quicksort.c
+++ code/quicksort.c
@@ -1,8 +1,9 @@
 #include "mergesort_RAM.c"
-#include "merge.c"
+#include "merge_qs.c"
 
 #include <stdlib.h>
 #include <sys/time.h>
+#include "constantes.h"
 
 
 /**
@@ -10,15 +11,13 @@
    *
    * Esta función
    * 
-   * @param A: puntero a un archivo a ordenar
-   * @param B: tamaño del bloques
-   * @param M: tamaño de RAM
+   * @param A: nombre del archivo a ordenar
    * @param a: cantidad de subarreglos
    * @param N: tamaño en bytes de A
    * @return: Int cantidad de accesos a memoria
    * @throws Termina el programa si no puede abrir el archivo o hay un error al crear los archivos de los subarreglos.
    */
-int Quicksort_externo( const char *A , int B, int M, int a, int N) {
+int Quicksort_externo( const char *A, int a, int N) {
     FILE* archivo = fopen(A, "rb+");
     if (!archivo) {
         perror("Error al abrir archivo de entrada");
@@ -31,8 +30,6 @@ int Quicksort_externo( const char *A , int B, int M, int a, int N) {
 
         int i_a= 0; //cantidad de enteros escritos en arreglo a ordenar 
         int i_arch= 0; //cantidad de enteros escritos en archivo final 
-        int E= B / sizeof(int64_t); //cantidad de enteros en un bloque
-
     
         for(int i=0; i< cant_bloques; i++){ //recooro los bloques
             int64_t *bloque_i = malloc(B);
@@ -46,7 +43,7 @@ int Quicksort_externo( const char *A , int B, int M, int a, int N) {
 
         int64_t* aux= malloc(N);
         mergesort_RAM(arreglo_a_ordenar, aux, 0, cant_num-1);
-
+        free(aux);
         fseek(archivo, 0, SEEK_SET);
         for(int i=0; i< cant_bloques-1; i++){
             int64_t *bloque_i = malloc(B);
@@ -66,79 +63,113 @@ int Quicksort_externo( const char *A , int B, int M, int a, int N) {
         free(bloque_i);
 
         fclose(archivo);
-
         free(arreglo_a_ordenar);
-        free(aux);
         int bloques = (N + B - 1) / B;
         return 2 * bloques; 
     }
 
-    else{ //N> M, elegir pivotes y dividir el archivo en subarreglos
-        
-        int E= B / sizeof(int64_t); //cantidad de números en un bloque
-        int64_t * bloque1= malloc(B); //leeremos el bloque 1 para elegir los pivotes
-        int leidos;
-        fread(bloque1, sizeof(int64_t), E, archivo);
-        int pivotes[a-1]; 
-        for (int i = 0, i<a, i++){
-            pivotes[i] = bloque1[i]
+    else{ //N> M, dividir el archivo en subarreglos
+        int lecturas = 0;
+        int cant_bloques = (N + B - 1) / B;
+        int64_t** arreglo_en_RAM = malloc(a * sizeof(int64_t*));// arreglo_en_RAM[a][E] para guardar los bloques de subarreglos
+        for (int i = 0; i < a; i++) {
+            arreglo_en_RAM[i] = malloc(E * T);
         }
-        int * pivotes_aux = malloc((a-1)* sizeof(int));
-        mergesort(pivotes, pivotes_aux, 0, a-1);
-
-        int arreglo_en_RAM[a][b]; 
-        int contadores_en_RAM[a][b];
-
         
-        FILE* subarreglos[a]; //creo a archivos para escribir cada subarreglo en disco
-        char nombre[32];
-        for (int i = 0; i < A; i++) {
-            snprintf(nombre, sizeof(nombre), "temp_%d.bin", i);  // genera nombre
-            subarreglos[i] = fopen(nombre, "wb");// crea archivo binario
+        int* cnt_sub_RAM = malloc(a * sizeof(int));// cnt_sub_RAM[a][E] para guardar los contadores de cada subarreglo
+        for (int i = 0; i < a; i++) {
+            cnt_sub_RAM[i] = 0; // Inicializamos cada contador en 0
+        }
+
+        char nombres_archivos[a][32];  // Arreglo 2D para guardar los nombres de los archivos
+        FILE* subarreglos[a];
+
+        for (int i = 0; i < a; i++) {
+            snprintf(nombres_archivos[i], sizeof(nombres_archivos[i]), "%d_%d.bin", A, i); // guarda el nombre
+            subarreglos[i] = fopen(nombres_archivos[i], "wb");
             if (subarreglos[i] == NULL) {
                 perror("Error al crear archivo");
-                for (int j = 0; j < i; j++) {//cerrar los archivos abiertos antes de salir
+                for (int j = 0; j < i; j++) {
                     fclose(subarreglos[j]);
                 }
                 exit(1);
             }
         }
 
-        //primer bloque en RAM
-        //recorro el bloque con i
-        for (int i = 0; i < E; i++) {
-            num= bloque1[i];
-            //guardo los elementos en el subarreglo correspondiente
-            //recorro pivotes con j
-            for (int j = 0; j < a; j++) {
-                if (num <= pivotes[j] || j==a-1){
-                    cantidad_de_elementos= contadores_en_RAM[j];
-                    arreglo_en_RAM[j][cantidad_de_elementos] = num;
-                    contadores_en_RAM[j]++;
+        
+        // Crear un arreglo de enteros para almacenar los bytes leídos de cada archivo
+        int* N_subarreglo = malloc(a * sizeof(int));
+        if (N_subarreglo == NULL) {
+            perror("Error al asignar memoria para N_subarreglo");
+            exit(1);
+        }
+        for (int i = 0; i < a; i++) {// Inicializar a cero (si es necesario)
+            N_subarreglo[i] = 0;
+        }
 
-                    //si el bloque se llena, lo guardo en el subarreglo
-                    if (contadores_en_RAM[j] == E){
-                        //escribo el bloque en el archivo[]
-                        fopen(subarreglos[j], "wb");
-                        fwrite(arreglo_en_RAM[j], sizeof(int), E, subarreglos[j]);
-                        fclose(subarreglos[j]);
-                        //vacio el bloque
-                        contadores_en_RAM[j] = 0;
-                    }
-                    break;
-                } 
+        for(int i=0; i< cant_bloques; i++){ //recooro los bloques
+            int64_t *bloque_i = malloc(B);
+            int leidos = fread(bloque_i, T, E, archivo);
+            lecturas +=1;
+            int64_t pivotes[a-1]; 
+            if (leidos > E) {
+                perror("Error leyendo bloque");
             }
+            if(i==0){// bloque_i es el primero, saco pivotes
+                for (int j = 0; j < a-1; j++){
+                    pivotes[j] = bloque_i[j];
+                }
+                int64_t * pivotes_aux = malloc((a-1)* T);
+                mergesort_RAM(pivotes, pivotes_aux, 0, a-2);
+                free(pivotes_aux);
+            }
+            for (int l= 0; l < leidos; l++) { //recorrer bloque en ram
+                int64_t num = bloque_i[l]; 
+                for (int j = 0; j < a; j++) { //recorrer pivotes, guardar el elemento en el subarreglo correspondiente
+                    if (num <= pivotes[j] || j==a-1){ //si el elemento es menor o igual al pivote, lo guardo en el subarreglo
+                        arreglo_en_RAM[j][cnt_sub_RAM[j]] = num;
+                        cnt_sub_RAM[j]++;
 
-        }   
+                        if (cnt_sub_RAM[j] == E){ //si el bloque se llena, lo guardo en el subarreglo
+                            fwrite(arreglo_en_RAM[j], T, cnt_sub_RAM[j], subarreglos[j]);//escribo el bloque en el subarreglo[]
+                            lecturas +=1;
+                            N_subarreglo[j] += B;
+                            cnt_sub_RAM[j] = 0;
+                        }
+                        break;
+                    } 
+                }
+                
+            }
+            free(bloque_i);
+        }
+        
+        for (int j = 0; j < a; j++) {//escribir los bloques restantes en disco
+            if (cnt_sub_RAM[j] > 0) { //si el bloque no está vacío
+                fwrite(arreglo_en_RAM[j], T, cnt_sub_RAM[j], subarreglos[j]);
+                lecturas +=1;
+                N_subarreglo[j] += cnt_sub_RAM[j] * T;
+            }
+        }
+        
+        for (int i = 0; i < a; i++) {
+            lecturas += Quicksort_externo(nombres_archivos[i], a, N_subarreglo[i]);
+        }
+        for (int i = 0; i < a; i++) {
+            free(arreglo_en_RAM[i]);
+            fclose(subarreglos[i]);
+        }
+        lecturas += merge_qs_externo(a, nombres_archivos, N_subarreglo, A);
+
+    
+        
+        free(arreglo_en_RAM);
+        free(cnt_sub_RAM);
+       
+        free(subarreglos);
+        free(N_subarreglo);
+        return lecturas;
     }
-
- 
-
-
-
-
     fclose(archivo);
-
-
-
+    
 }
