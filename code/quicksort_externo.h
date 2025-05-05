b492f7db95c922749411da900866c8fea887c40f
unknown <paul.holmer789@gmail.com>
2025-05-05 18:31:37 -0400

    feat: delete code folder

--- code/quicksort_externo.h
+++
@@ -1,6 +0,0 @@
-#include "constantes.h"
-
-char *quitar_extension_bin(const char* nombre);
-int quicksort_externo(const char *A, int a, int64_t N);
-int concatenar_externo(int n, char nombres[][32], int64_t tamanos[], const char *A);
-void quicksort_RAM(int64_t* arreglo, int inicio, int fin);
